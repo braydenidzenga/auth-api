cbcd28c103ed4b6047673c3fb084a07400953b1b
Brayden Idzenga <braydenidzenga87@gmail.com>
2024-05-09 02:42:28 -0400

    valid app secret required for all requests

--- routes/auth.js
+++ routes/auth.js
@@ -2,28 +2,79 @@ import express from "express";
 import signUp from "./auth/signup.js";
 import { loginWithUsername, loginWithEmail } from "./auth/login.js";
 import { verifyToken } from "../util/token.js";
+import { verifySecret } from "../util/appSecret.js";
 
 const router = express.Router();
 
 router.post("/signup", async (req,res) => {
+    const appSecret = req.body.appSecret;
+
+    try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
     const { statusCode, resMessage } = await signUp(req);
     return res.status(statusCode).json(resMessage); 
 });
 
 router.post("/loginWithUsername", async (req,res) => {
+    const appSecret = req.body.appSecret;
+    try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
     const { statusCode, resMessage } = await loginWithUsername(req);
     return res.status(statusCode).json(resMessage);
 });
 
 router.post("/loginWithEmail", async (req,res) => {
+    const appSecret = req.body.appSecret;
+    try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
     const { statusCode, resMessage } = await loginWithEmail(req);
     return res.status(statusCode).json(resMessage);
 });
 
 router.post("/getuser", async (req,res) => {
-    const { token } = req.body;
+    const { token, appSecret } = req.body;
 
     try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+
         const user = await verifyToken(token);
         if (user === null) {
             return res.status(400).json({
--- routes/settings.js
+++ routes/settings.js
@@ -1,15 +1,46 @@
 import express from "express";
 import changePassword from "./settings/changePassword.js";
 import changeUsername from "./settings/changeUsername.js";
+import { verifySecret } from "../util/appSecret.js";
 
 const router = express.Router();
 
 router.post("/changePassword", async (req,res) => {
+    const appSecret = req.body.appSecret;
+
+    try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
     const { statusCode, resMessage } = await changePassword(req);
     return res.status(statusCode).json(resMessage);
 });
 
 router.post("/changeUsername", async (req,res) => {
+    const appSecret = req.body.appSecret;
+
+    try {
+        const valid = await verifySecret(appSecret);
+        if (!valid) {
+            return res.status(400).json({
+                message: "Not authorized"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+
     const { statusCode, resMessage } = await changeUsername(req);
     return res.status(statusCode).json(resMessage);
 });
